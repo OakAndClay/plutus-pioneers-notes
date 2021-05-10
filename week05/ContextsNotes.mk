5eabb49666a8bb2964a59baa72c59ef8a9137055
OakAndClay <81421380+OakAndClay@users.noreply.github.com>
2021-05-09 21:08:39 -0400

    Create ContextsNotes.mk

---
+++ week05/ContextsNotes.mk
@@ -0,0 +1,46 @@
+*Validation Rehash
+  * If
+    * Dont have a public key address
+    * Do have script address
+    * Do have a UTXO
+    * So have a transaction that tries to consume the UTXO as an imput
+  * Then
+    * For each script input the corresponding script will run
+    
+  * Script takes inputs
+    * Datum
+    * Redeemer
+    * Context
+    
+* Context is defined in [Contexts.hs](https://github.com/input-output-hk/plutus/blob/master/plutus-ledger-api/src/Plutus/V1/Ledger/Contexts.hs)
+```
+data ScriptContext = ScriptContext{scriptContextTxInfo :: TxInfo, scriptContextPurpose :: ScriptPurpose }
+```
+* ScriptContext has two fields that are defined
+  * TxInfo
+```
+-- | A pending transaction. This is the view as seen by validator scripts, so some details are stripped out.
+data TxInfo = TxInfo
+    { txInfoInputs      :: [TxInInfo] -- ^ Transaction inputs
+    , txInfoInputsFees  :: [TxInInfo]     -- ^ Transaction inputs designated to pay fees
+    , txInfoOutputs     :: [TxOut] -- ^ Transaction outputs
+    , txInfoFee         :: Value -- ^ The fee paid by this transaction.
+    , txInfoForge       :: Value -- ^ The 'Value' forged by this transaction.
+    , txInfoDCert       :: [DCert] -- ^ Digests of certificates included in this transaction
+    , txInfoWdrl        :: [(StakingCredential, Integer)] -- ^ Withdrawals
+    , txInfoValidRange  :: SlotRange -- ^ The valid range for the transaction.
+    , txInfoSignatories :: [PubKeyHash] -- ^ Signatures provided with the transaction, attested that they all signed the tx
+    , txInfoData        :: [(DatumHash, Datum)]
+    , txInfoId          :: TxId
+    -- ^ Hash of the pending transaction (excluding witnesses)
+    } deriving (Generic)
+```
+  * ScriptPurpose
+```
+-- | Purpose of the script that is currently running
+data ScriptPurpose
+    = Minting CurrencySymbol
+    | Spending TxOutRef
+    | Rewarding StakingCredential
+    | Certifying DCert
+```
